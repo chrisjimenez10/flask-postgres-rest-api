ed3d129c41f56fbea7d0aa382998f372da03abb4
chris <jimenezchristopher5@gmail.com>
2024-06-26 21:13:46 -0500

    added CORS to allow incoming requests from any Domain

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from flask import Flask, request, jsonify
 #We can access the environment variables from the .env file through load_env()
 from dotenv import load_dotenv
+from flask_cors import CORS
 import os
 import psycopg2, psycopg2.extras
 
@@ -19,6 +20,8 @@ load_dotenv()
 # Initialize Flask
 # We'll use the pre-defined global '__name__' variable to tell Flask where it is.
 app = Flask(__name__)
+#Here, we are adding CORS ABOVE the Routes and BELOW the initializaiton of our Flask App --> This way we can access our api from any Domain
+CORS(app)
 
 # Define our route
 # This syntax is using a Python decorator, which is essentially a succinct way to wrap a function in another function.
